1bb64291186142622d47589033a45850040ebe8e
Martin <martin.bogusz@hotmail.com>
2024-03-12 17:06:23 +0100

    add timeline

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -14,9 +14,9 @@ export function App() {
       <div className='flex flex-row text-center justify-center flex-grow relative'>
         <div className='flex flex-col justify-center'>
     <div className='fixed top-0 mt-12 flex flex-row justify-between font-inter text-base'>
-      <div className='mr-8 font-bold'>About me</div>
-      <div className='mr-8'>Background</div>
-      <div className='mr-8'>Projects</div>
+      <a className='mr-8 font-bold'>About me</a>
+      <a className='mr-8'>Background</a>
+      <a className='mr-8'>Projects</a>
 
     </div>
         <div className='font-bold text-[86px] font-inter text-left mb-12 leading-[106px]'>
@@ -31,13 +31,24 @@ Fast forward to today, this fascination turned into a career.<br></br>
         </div>
 
         </div>
+        <div className='fixed bottom-0 border-r-2 border-black h-[30%]' >
+          <div className='-mt-[2px] -mr-[6px] rounded-full h-3 w-3 bg-blue'></div>
+        </div>
       </div>
-      <div className="flex-initial w-[40vw] h-[80vh]   overflow-hidden">
+      <div className='flex flex-col'>
                 <img
-                  className="cursor-pointer object-cover rounded-bl-[120px]"
+                  className="w-[40vw] h-[80vh] cursor-pointer object-cover rounded-bl-[120px]"
                   src="/me/portrait-martin2.jpg"
                 ></img>
-              </div>
+                  <div className='pl-16 mt-8 flex flex-row  font-inter text-base justify-center'>
+                <a className='mr-8 pr-8 font-bold border-r-2 border-black'>Software Engineering</a>
+              <a className='mr-8 pr-8 marker: border-r-2 border-black'>Performance</a>
+              <a className='mr-8'>Security</a>
+
+                  
+                </div>
+
+      </div>
  </div>
   );
 }
